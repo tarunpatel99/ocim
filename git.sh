dbdc2978d0fe1959949091cc3898d7775fc11237
harshilsureja <harshilsureja@gmail.com>
2019-11-03 17:57:06 +0530

    Description 	: code publish using sh ================================================== Date and Time 	: Sun Nov  3 17:57:06 IST 2019

---
+++ git.sh
@@ -0,0 +1,15 @@
+#file in ocim
+clear
+echo "Enter the description for commit : \c"
+read commit
+date=`date`
+echo "=================================================="
+comment=`echo "Description 	: $commit \n==================================================\nDate and Time 	: $date"`
+echo "$comment"
+echo "=================================================="
+git add .
+git status
+git commit -m "$comment"
+git push
+
+
