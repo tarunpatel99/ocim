1f8a43a8fca4df61e8a888e6ca82110d7e2e0de0
Tarun <tarunpatel99@users.noreply.github.com>
2019-11-24 19:12:49 +0530

    Express JS and backend connectivity
    -> ExpreesJS
        npm install --save express
    
    ---------------------------------------
    
    -> Auto restart server
       npm install --save-dev nodemon
    
       commnad for un serve
       "npm run start:server"

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,36 @@
+// Install ExpressJS by follwing command
+// npm install --save express
+
 const express = require('express');
 
 const app = express();
 
+app.use('/students', (req, res, next) => {
+    const student = [
+        {
+            id: "17bca068",
+            name: "Harshil Sureja",
+            class: "TYBCA",
+            city: "Ahmedabad"
+        },
+        {
+            id: "17bca140",
+            name: "Tarun Patel",
+            class: "TYBCA",
+            city: "Ahmedabad"
+        },
+        {
+            id: "17bca140",
+            name: "Aakash Bhavsar",
+            class: "TYBCA",
+            city: "Ghandhinagar"
+        },
+    ];
 
+    res.status(200).json({
+        message: "Data get successfully",
+        students: student
+    });
+});
+
+module.exports = app;
