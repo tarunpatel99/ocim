90bfd03165252944a28201361a115ead13cb6dfe
Tarun <tarunpatel99@users.noreply.github.com>
2019-11-26 19:54:00 +0530

    5 Components, Auth and Entities

---
+++ src/app/student.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { ThrowStmt } from '@angular/compiler';
+import { Subject } from 'rxjs';
+
+export interface StudentModel {
+  id: string;
+  name: string;
+  class: string;
+  city: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class StudentService {
+  StudentDataTable: StudentModel[] = []
+  private StudentUpdated = new Subject<StudentModel[]>();
+
+  constructor(private http: HttpClient) { }
+
+  getStudents() {
+    this.http.get<{message: string, students: StudentModel[]}>('http://localhost:3000/students')
+      .subscribe((studentData) => {
+        this.StudentDataTable = studentData.students
+        this.StudentUpdated.next([...this.StudentDataTable]);
+        
+      });
+      console.log(this.StudentDataTable)
+    console.log("services are working");
+  }
+
+  getStudentsUpdateListener() {
+    return this.StudentUpdated.asObservable();
+  }
+}
