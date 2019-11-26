0a4426d9e6a963682dbd85ce5486121e040c8dd2
harshilsureja <harshilsureja@gmail.com>
2019-11-26 22:25:09 +0530

    Description 	: demo sh file nothing work here ================================================== Date and Time 	: Tue 26 Nov 22:25:09 IST 2019

--- git.sh
+++ git.sh
@@ -1,15 +1,12 @@
 #file in ocim
 clear
-echo "Enter the description for commit : \c"
-read commit
-date=`date`
-echo "=================================================="
-comment=`echo "Description 	: $commit \n==================================================\nDate and Time 	: $date"`
-echo "$comment"
-echo "=================================================="
-git add .
-git status
-git commit -m "$comment"
-git push
-
+echo "1) Push to GitHub"
+echo "2) Fetch From GitHub"
+echo "Enter the Choice: \c"
+read choice
+case "$choice" in
+1) sh gitpush.sh;;
+2) sh gitpull.sh;;
+*) echo "Invalid Choice :("
+esac
 
