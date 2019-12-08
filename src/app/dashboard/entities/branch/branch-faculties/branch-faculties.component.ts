4d406e261db558077586e71d1822262335059b55
harshilsureja <harshilsureja@gmail.com>
2019-12-08 19:26:48 +0530

    Description 	: faculty table and ux change ================================================== Date and Time 	: Sun  8 Dec 19:26:47 IST 2019

--- src/app/dashboard/entities/branch/branch-faculties/branch-faculties.component.ts
+++ src/app/dashboard/entities/branch/branch-faculties/branch-faculties.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Action } from 'rxjs/internal/scheduler/Action';
 
 export interface Class {
   value: string;
@@ -19,6 +20,31 @@ export interface State {
   value: string;
   viewValue: string;
 }
+
+
+export interface Fac_element {
+  name: string;
+  position: number;
+  email: string;
+  phone: string;
+
+
+}
+
+const Faculty_Data: Fac_element[] = [
+  {position: 1, name: 'Faculty Middlename Surname', email: 'facultyemail1@gmail.com', phone: '9876543210'},
+  {position: 2, name: 'Faculty Middlename Surname', email: 'facultyemail2@gmail.com', phone: '9876543210'},
+  {position: 3, name: 'Faculty Middlename Surname', email: 'facultyemail3@gmail.com', phone: '9876543210'},
+  {position: 4, name: 'Faculty Middlename Surname', email: 'facultyemail4@gmail.com', phone: '9876543210'},
+  {position: 5, name: 'Faculty Middlename Surname', email: 'facultyemail5@gmail.com', phone: '9876543210'},
+  {position: 6, name: 'Faculty Middlename Surname', email: 'facultyemail6@gmail.com', phone: '9876543210'},
+  {position: 7, name: 'Faculty Middlename Surname', email: 'facultyemail7@gmail.com', phone: '9876543210'},
+  {position: 8, name: 'Faculty Middlename Surname', email: 'facultyemail8@gmail.com', phone: '9876543210'},
+  {position: 9, name: 'Faculty Middlename Surname', email: 'facultyemail9@gmail.com', phone: '9876543210'},
+  {position: 10, name: 'Faculty Middlename Surname', email: 'facultyemail10@gmail.com', phone: '9876543210'},
+];
+
+
 
 @Component({
   selector: 'app-branch-faculties',
@@ -56,6 +82,11 @@ export class BranchFacultiesComponent implements OnInit {
     {value: 'Maharastra', viewValue: 'Maharastra'}
   ];
   
+
+  displayedColumns: string[] = ['name', 'email', 'phone','action'];
+  dataSource = Faculty_Data;
+
+
   ngOnInit() {
   }
 
