b1837fbef7351efaa82b357ec925461afcb7be7c
Harshil <harshilsureja@gmail.com>
2019-12-26 16:59:30 +0530

    Description 	: harshilsureja ================================================== Date and Time 	: Thu 26 Dec 16:59:30 IST 2019

--- src/app/dashboard/entities/branch/branch-leave-notes/branch-leave-notes.component.ts
+++ src/app/dashboard/entities/branch/branch-leave-notes/branch-leave-notes.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, Inject } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { FormControl } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material';
+
 
 export interface Attatchment {
   id: string
@@ -25,8 +27,8 @@ export interface LeaveNote {
   // leave notes data
   const leaves: LeaveNote[] = [
     { id: "132", class: "8", studentName: "Tarun Patel", subject: "Khabar nai", description: "Dr. ne mali lo", leaveFrom: "12/12/2019", leaveTo: "22/12/2019", attatchment: [{ id: "Attatchment one" }, { id: "Attatchment two" }], status: "Pending" },
-    { id: "132", class: "8", studentName: "Tarun Patel", subject: "Khabar nai", description: "Dr. ne mali lo", leaveFrom: "12/12/2019", leaveTo: "22/12/2019", attatchment: [{ id: "Attatchment one" }, { id: "Attatchment two" }], status: "Approved" },
-    { id: "126", class: "8", studentName: "Tarun Patel", subject: "Khabar nai", description: "Dr. ne mali lo", leaveFrom: "12/12/2019", leaveTo: "22/12/2019", attatchment: [{ id: "Attatchment one" }, { id: "Attatchment two" }], status: "Denied" },
+    { id: "132", class: "9", studentName: "Tarun Patel", subject: "Khabar nai", description: "Dr. ne mali lo", leaveFrom: "12/12/2019", leaveTo: "22/12/2019", attatchment: [{ id: "Attatchment one" }, { id: "Attatchment two" }], status: "Approved" },
+    { id: "126", class: "7", studentName: "Tarun Patel", subject: "Khabar nai", description: "Dr. ne mali lo", leaveFrom: "12/12/2019", leaveTo: "22/12/2019", attatchment: [{ id: "Attatchment one" }, { id: "Attatchment two" }], status: "Denied" },
   ]
   
 
@@ -37,9 +39,10 @@ export interface LeaveNote {
   styleUrls: ['./branch-leave-notes.component.css']
 })
 export class BranchLeaveNotesComponent implements OnInit {
+  selectedLeave: any;
+ 
 
-
-  constructor() {
+  constructor(public dialog: MatDialog) {
     this.dataSource = new MatTableDataSource(leaves);
   }
 
@@ -73,6 +76,56 @@ export class BranchLeaveNotesComponent implements OnInit {
       case 'Denied':
         return '#C62828'; // red
     }
+  }
+
+
+
+
+  // open dialog box
+  onViewLeaveNote(event, leave): void {
+    this.selectedLeave = leave
+    const dialogRef = this.dialog.open(LeaveNoteFormDialog, {
+      width: '500px',
+      data: { studentName: this.selectedLeave.studentName , 
+              class: this.selectedLeave.class , 
+              subject: this.selectedLeave.subject, 
+              description: this.selectedLeave.description, 
+              from: this.selectedLeave.leaveFrom, 
+              to: this.selectedLeave.leaveTo , 
+              attatchments: this.selectedLeave.attatchments}
+    });
+    
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+
+
+
+}
+
+
+
+
+
+// view leave note details dialog box
+@Component({
+  selector: 'dialog-leave-note',
+  templateUrl: 'leave-note.form.html',
+  styleUrls: ['leave-note.form.css']
+})
+export class LeaveNoteFormDialog implements OnInit{
+  constructor(
+    public dialogRef: MatDialogRef<LeaveNoteFormDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: LeaveNote) { }
+
+  onSave(): void {
+    this.dialogRef.close();
+  }
+
+  ngOnInit() {
+    
   }
 
 }
