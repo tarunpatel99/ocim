f997e5bed0c403b090272e31116a1857dd7903c2
Tarun <32832014+tarunpatel99@users.noreply.github.com>
2020-01-11 13:05:03 +0530

    Admin updated
    Institutes list added
    Payment history added

--- src/app/dashboard/entities/admin/admin-institutes/admin-institutes.component.ts
+++ src/app/dashboard/entities/admin/admin-institutes/admin-institutes.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
+import { FormControl } from '@angular/forms';
+
+export interface institute {
+  id?: String;
+  name: string;
+  email: string;
+  phone: string;
+  branches: number;
+  faculties: number;
+  students: number;
+}
 
 @Component({
   selector: 'app-admin-institutes',
@@ -7,9 +19,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminInstitutesComponent implements OnInit {
 
+  institutes: institute[] = [
+    { name: 'GyanJyot', email: 'contact@gyanjyot.com', phone: '9999888776', branches: 7, faculties: 50, students: 1500, },
+    { name: 'Sandip Patel', email: 'contact@sp.com', phone: '6778889999', branches: 3, faculties: 32, students: 900, }
+  ]
+
   constructor() { }
+  displayedColumns: string[] = ['name', 'email', 'phone', 'branches', 'faculties', 'students', 'action'];
+  dataSource = new MatTableDataSource<institute>(this.institutes);
+
+
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  date = new FormControl(new Date());
+
+
+
 
   ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+
+
+  // filtering data
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
 }
