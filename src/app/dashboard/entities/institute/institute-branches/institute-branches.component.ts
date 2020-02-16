7b78d6db4c914629550cbd0a7a6133b54e41c973
Aakash <54474266+AakashBhavsar@users.noreply.github.com>
2020-02-16 17:04:55 +0530

    Owner Reports

--- src/app/dashboard/entities/institute/institute-branches/institute-branches.component.ts
+++ src/app/dashboard/entities/institute/institute-branches/institute-branches.component.ts
@@ -5,6 +5,7 @@ import {MatTableDataSource} from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
 import { FormControl } from '@angular/forms';
 import {FormBuilder, FormGroup, Validators, MaxLengthValidator} from '@angular/forms';
+import { PrintService } from 'src/app/dashboard/printservice/print.service';
 
 
 export interface City {
@@ -62,7 +63,27 @@ export class InstituteBranchesComponent implements OnInit {
     Validators.required
   ]);
 
-  constructor() { }
+  branchList = []
+  Branch_Data = [
+    {branchname: 'House Bopal' , name: 'Haresh Oza', email: 'haresh1976@gmail.com' , phone: '9876543210'},
+    {branchname: 'Ghuma' , name: 'Ankti Patel', email: 'pAnkti86@gmail.com' , phone: '8644756557'},
+    {branchname: 'South Bopal' , name: 'Kaushik Prajapti', email: 'kaushik@gmail.com' , phone: '9876543210'},
+    {branchname: 'Science city' , name: 'Vinay Sureja', email: 'dSureja@gmail.com' , phone: '6845652133'},
+    {branchname: 'Maninagar' , name: 'Dharmesh Jadeja', email: 'dharmeshj@gmail.com' , phone: '8665645212'},
+    {branchname: 'SBI Bopal' , name: 'Chinmay Bhatt', email: 'bhattChiku@gmail.com' , phone: '6845645102'},
+    {branchname: 'Bapunagar' , name: 'Nirav Kadia', email: 'kNirav@gmail.com' , phone: '7565422388'},
+    {branchname: 'Nerunagar' , name: 'Shivam Patel', email: 'shivampatel@gmail.com' , phone: '8648546217'},
+    {branchname: 'Iscon' , name: 'Shubh Bhatt', email: 'shubhb@gmail.com' , phone: '8456512378'},
+    {branchname: 'Vastrapur' , name: 'Shubham Mevada', email: 'mevadaS@gmail.com' , phone: '9643532123'},
+  ]; ;
+  constructor(private PrintService: PrintService) {
+    for (let i = 0; i < this.Branch_Data.length; i++) {
+      if (!this.branchList.includes(this.Branch_Data.map(branchname => branchname.branchname)[i]))
+        this.branchList.push(this.Branch_Data.map(branchname => branchname.branchname)[i])
+    }
+    // Assign the data to the data source for the table to render
+    this.dataSource = new MatTableDataSource(this.Branch_Data);
+  }
 
   checked = true;
   
@@ -129,6 +150,13 @@ export class InstituteBranchesComponent implements OnInit {
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
+  }
+  generateReport() {
+    // table column to display in pdf
+    let columns: string[] = ['branchname', 'name', 'email', 'phone'];
+
+    // this.PrintService.generateReport(data, columns, pdftitle)
+    this.PrintService.generateReport(this.dataSource.filteredData, columns, 'Branch Details')
   }
 
 }
