18928abb97d80c18dc76e2784c583e547d8423cf
Aakash <54474266+AakashBhavsar@users.noreply.github.com>
2020-02-21 10:42:10 +0530

    receipt

--- src/app/dashboard/entities/student/student-fees/student-fees.component.ts
+++ src/app/dashboard/entities/student/student-fees/student-fees.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
-declare let jsPDF: new () => any;
+import * as jsPDF from 'jspdf';
+import 'jspdf-autotable';
+import {UserOptions} from 'jspdf-autotable'
+
+interface jsPDFWithPlugin extends jsPDF {
+  autoTable : (options : UserOptions) => jsPDF;
+}
 
 export interface StudentFees {
   std: number;
@@ -10,12 +16,7 @@ export interface StudentFees {
   amount: number;
   status: string;
 }
-var columns = ["ID", "Name", "Country"];
-var rows = [
-    [1, "Shaw", "Tanzania"],
-    [2, "Nelson", "Kazakhstan"],
-    [3, "Garcia", "Madagascar"],
-];
+
 const students: StudentFees[] = [
   { std: 8 , sem: 2, mode: 'Online', date: '29-12-2019', amount: 45000, status: "Unpaid"},
   { std: 8 , sem: 1, mode: 'Online', date: '29-12-2019', amount: 45000, status: "Paid"},
@@ -36,9 +37,69 @@ export class StudentFeesComponent implements OnInit {
   }
 
   onPrint() {
-    let doc = new jsPDF();
-        doc.autoTable(columns, rows);
-        doc.save("table.pdf");
+    let doc = new jsPDF("p", "pt", "a4") as jsPDFWithPlugin;
+    doc.setFont("arial", "bold");
+    doc.setFontSize(23);
+    doc.text(180,18,'Gyanjyot Institute')
+    doc.setFont("arial", "normal");
+    doc.setFontSize(12);
+    doc.text(220,35,'SBI Bopal Branch');
+    doc.setLineCap(2);
+    doc.setFont("arial", "bold");
+    doc.setFontSize(15);
+    doc.text(40,75,'Payment Receipt');
+    doc.setFontSize(10);
+    doc.text(40,600,"NOTE: THIS IS A COMPUTER-GENERATED DOCUMENT AND IT DOES NOT REQUIRE A SIGNATURE.");
+    //doc.line(15, 15, 200, 15);
+    doc.line(40, 80, 550, 80);
+    doc.line(40, 100, 550, 100);
+    doc.line(40, 340,550, 340);
+    doc.line(40, 360,550, 360);
+    doc.line(40, 520,550, 520);
+    
+    doc.autoTable({
+      margin: {top:80},
+      theme: "plain",
+      tableWidth: 'auto',
+			styles: {
+        minCellHeight: 30
+			},
+      
+      head:[['REC. NO.','1','DATE: 01-04-2019']],
+      body:[
+        ['Regn No.','1'],
+        ['Student:','Ajay Malhotra'],
+        ['Father:','Mr. Vinay Malhotra'],
+        ['Class:','3rd'],
+        ['Session:','JUNE 2020'],
+        ['Sem:','2'],
+        ['Receipt Number','705']
+      ]
+    })    
+    doc.autoTable({
+      theme: "plain",
+      tableWidth: 'auto',
+			styles: {
+        minCellHeight: 30
+			},
+      head:[['S.N              ','Particulars','Amount']],
+      body:[
+        ['1','Admissison Fees','15000'],
+        ['2','Tution Fees','15000'],
+        ['3','Computer Fees', '1000'],
+        ['4','Enrollment Fees','200'],
+        ['5','Exam and Stationary','500']
+      ],
+      foot:[['','TOTAL AMOUNT','31700']]
+    })
+    let win = open('about:blank');
+    let body = win.document.body;
+    body.setAttribute('style', 'height:100%; width:100%; padding:0px; margin: 0');
+    let iframe = document.createElement('iframe');
+    iframe.setAttribute('style', 'height:100%; width:100%; padding:0px; margin: 0; border: 0');
+    body.appendChild(iframe);
+
+    iframe.src = doc.output('datauristring');
   }
 
   getColor(status) {
