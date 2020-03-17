57093fefefabbb77067c5d90c76224030c76b442
Aakash <54474266+AakashBhavsar@users.noreply.github.com>
2020-03-17 21:59:20 +0530

    student schedule

--- src/app/dashboard/entities/student/student-exam-schedule/student-exam-schedule.component.ts
+++ src/app/dashboard/entities/student/student-exam-schedule/student-exam-schedule.component.ts
@@ -6,6 +6,8 @@ export interface StudentData {
   description: string;
   exmdate: string;
   subject: string;
+  start_time: string;
+  end_time: string;
   ttl_marks: number; // totla marks
   passing_mark: number;
 }
@@ -18,17 +20,17 @@ export interface StudentData {
 
 export class StudentExamScheduleComponent implements OnInit {
   students: StudentData[] = [
-    { description: 'Unit 1, 2' , exmdate: '14/12/2019', subject: 'Maths', ttl_marks: 50, passing_mark: 17 },
-    { description: 'Unit 1' , exmdate: '15/12/2019', subject: 'Science', ttl_marks: 50, passing_mark: 17 },
-    { description: 'Unit 1, 2, 3' , exmdate: '16/12/2019', subject: 'English', ttl_marks: 50, passing_mark: 17 },
-    { description: 'Unit 1, 2' , exmdate: '17/12/2019', subject: 'SST', ttl_marks: 50, passing_mark: 17 },
+    { description: 'Unit 1, 2' , exmdate: '14/12/2019', subject: 'Maths', start_time: '10:00 AM', end_time: '11:30 AM',ttl_marks: 50, passing_mark: 17 },
+    { description: 'Unit 1' , exmdate: '15/12/2019', subject: 'Science', start_time: '10:00 AM', end_time: '11:30 AM', ttl_marks: 50, passing_mark: 17 },
+    { description: 'Unit 1, 2, 3' , exmdate: '16/12/2019', subject: 'English',start_time: '12:00 PM', end_time: '01:30 PM', ttl_marks: 50, passing_mark: 17 },
+    { description: 'Unit 1, 2' , exmdate: '17/12/2019', subject: 'SST',start_time: '12:00 PM', end_time: '01:30 PM', ttl_marks: 50, passing_mark: 17 },
   ]
 
   constructor() {
     this.dataSource = new MatTableDataSource(this.students);
   }
 
-  displayedColumns: string[] = ['exmdate', 'subject', 'description','ttl_marks' , 'passing_mark'];
+  displayedColumns: string[] = ['exmdate', 'subject','start_time', 'end_time', 'description','ttl_marks' , 'passing_mark'];
   dataSource: MatTableDataSource<StudentData>;
   
 
